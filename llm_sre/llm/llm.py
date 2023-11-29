1037df4f1e24904ee77379269e47f06ce59c65e6
Ana Martínez Saucedo <anmartinez@uade.edu.ar>
2023-11-29 16:02:39 -0300

    wip: RAG

--- llm_sre/llm/llm.py
+++ llm_sre/llm/llm.py
@@ -3,40 +3,44 @@ from typing import Any
 from deepeval.evaluator import assert_test
 from deepeval.metrics import BaseMetric
 from deepeval.test_case import LLMTestCase
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import GPT4AllEmbeddings
 from langchain.llms import LlamaCpp
-from langchain.memory.chat_memory import BaseChatMemory
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain.chains import LLMChain
+from langchain.vectorstores import Chroma, VectorStore
 
 
 class LLM:
     _llm: LlamaCpp
-    _memory: BaseChatMemory
     _prompt: ChatPromptTemplate
+    _prompt_template: HumanMessagePromptTemplate
     _chain: LLMChain
     _input: dict
     _metrics: list[BaseMetric]
+    _vector_store: VectorStore = None
     _evaluate = bool
 
     def __init__(
         self,
         llm: LlamaCpp,
         prompt: ChatPromptTemplate,
-        memory: BaseChatMemory,
         prompt_template: HumanMessagePromptTemplate,
         metrics: list[BaseMetric],
         evaluate: bool,
+        requirements_file_path: str = None,
     ):
         self._llm = llm
-        self._memory = memory
         self._prompt = prompt
-        self._chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
         self._prompt_template = prompt_template
+        self._chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
         self._metrics = metrics
         self._evaluate = evaluate
+        if requirements_file_path:
+            document_loader = UnstructuredFileLoader(requirements_file_path)
+            document = document_loader.load()
             self._vector_store = Chroma.from_documents(documents=document, embedding=GPT4AllEmbeddings())
 
-    def prompt(self, **args):
     def prompt(self, *args) -> Any:
         self._prepare_prompt(*args)
         prompt = self._prompt_template.format(**self._input).content
@@ -52,3 +56,10 @@ class LLM:
 
     def _prepare_prompt(self, *args):
         pass
+
+    def _post_process_output(self, output: str) -> Any:
+        pass
+
+    def _run_test(self, case_input: str, output: str, context: str):
+        test_case = LLMTestCase(input=case_input, actual_output=output, context=[context])
+        assert_test(test_case, self._metrics)
